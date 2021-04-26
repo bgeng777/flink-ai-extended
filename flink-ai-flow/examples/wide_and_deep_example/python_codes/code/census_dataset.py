d9d269cff484eb51269f263bc6368af5268f7f61
bgeng777 <gengbiao.gb@alibaba-inc.com>
2021-04-26 20:01:01 +0800

    add license

--- flink-ai-flow/examples/wide_and_deep_example/python_codes/code/census_dataset.py
+++ flink-ai-flow/examples/wide_and_deep_example/python_codes/code/census_dataset.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 """Download and clean the Census Income Dataset."""
 
 from __future__ import absolute_import
