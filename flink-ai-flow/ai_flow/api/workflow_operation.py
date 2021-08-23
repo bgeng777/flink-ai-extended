dea9faf924f5609a6397ed5e91eff37aecb74191
bgeng777 <gengbiao.gb@alibaba-inc.com>
2021-08-23 14:44:15 +0800

    update licence

--- flink-ai-flow/ai_flow/api/workflow_operation.py
+++ flink-ai-flow/ai_flow/api/workflow_operation.py
@@ -14,7 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import cloudpickle
 
 from ai_flow.workflow.control_edge import EventCondition
 from typing import Text, List, Optional
